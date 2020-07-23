27da0f77cac89768488ba4f45b2860beedd066e9
Lucas Ventuala <ventualalucas@gmail.com>
2020-07-22 21:42:03 -0300

    mostrar datos personales

--- src/components/private/Home.vue
+++ src/components/private/Home.vue
@@ -1,13 +1,61 @@
 <template>
     <div>
-        HOME
+        <template v-if="$store.getters.getUser == null || $store.getters.getUser.estado == 1">
+            <div class="alert alert-warning" role="alert">
+                <h4 class="alert-heading">Atención!</h4>
+                <hr>
+
+                <p>El administrador no ha activado tu usuario!!</p>
+                <p>En cuanto lo haga vas a poder visualizar las funciones del sistemas.</p>
+            </div>
+        </template>
+        <template v-else>
+            <div class="card shadow p-3 mb-5 bg-white rounded w-75 mx-auto">
+                <div class="card-header">
+                    Información del usuario
+                </div>
+                <div class="card-body">
+                    <form action="#">
+                        <div class="form-group">
+                            <label for="nombre">Nombre:</label>
+                            <input
+                                name="nombre" type="text"
+                                placeholder=""
+                                id="nombre"
+                                v-bind:value="$store.getters.getUser.nombre"
+                                class="form-control"
+                            >
+                            <div class="invalid-feedback">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email:</label>
+                            <input
+                                name="email" type="text"
+                                placeholder=""
+                                id="email"
+                                v-bind:value="$store.getters.getUser.email"
+                                class="form-control"
+                            >
+                            <div class="invalid-feedback">
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </template>
+    
     </div>
 </template>
 
 <script>
-export default {
+import { mapState,mapActions } from "vuex";
 
+export default {
 }
+
 </script>
 
 <style>
--- src/components/private/PedidosGenerados.vue
+++ src/components/private/PedidosGenerados.vue
@@ -1,7 +1,52 @@
 <template>
-  <div>
-    {{ pedidos }}
-  </div>
+      <div class="card shadow p-3 mb-5 bg-white rounded">
+        <div class="card-header">
+            Pedidos Generados
+        </div>
+        <div class="card-body">
+
+            
+
+            <div id="accordion">
+
+                @foreach($pedidos as $pedido)
+
+                <div v-for="pedido in pedidos" :key="pedido.id" class="card">
+                    <div class="card-header" :id="'id_'+pedido.id">
+                        <h5 class="mb-0">
+                            <button class="btn btn-link" data-toggle="collapse" :data-target="'#collapse_'+pedido.id" aria-expanded="true" :aria-controls="'collapse_'+pedido.id">
+                                Pedido #{{pedido.id}}
+                            </button>
+                        </h5>
+                    </div>
+
+                    <div :id="'collapse_'+pedido.id" class="collapse show" :aria-labelledby="'id_'+pedido.id" data-parent="#accordion">
+                        <div class="card-body">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Nombre</th>
+                                        <th scope="col">Descripcion</th>
+                                        <th scope="col">Imagen</th>
+                                        <th scope="col">Cantidad Pedida</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="producto in pedido.productos" :key="producto.id">
+                                        <td>{{producto.nombre}}</td>
+                                        <td>{{producto.descripcion}}</td>
+                                        <td><img :src="'data:image/gif;base64,'+producto.img" class="img_Thumbnail"></td>
+                                        <td>{{producto.cantidad}}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
